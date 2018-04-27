21a6c9d21bcdeb53c48bbdd25e2b58f973ef94e2
maxwshen <maxwshen@gmail.com>
2018-04-27 19:21:17 -0400

    including model

--- indelphi_app.py
+++ indelphi_app.py
@@ -2,12 +2,26 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+# Dash server setup
 app = dash.Dash('')
 server = app.server
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-
+##
+# App layout
+##
 app.layout = html.Div([
+  html.H1('inDelphi'),
+  html.Div('Description'),
+
+  # Input text box 1
+  html.Label('DNA sequence 1'),
+  dcc.Input(id = 'textbox1', value = 'ACGT', type = 'text'),
+
+  # Input text box 2
+  html.Label('DNA sequence 2'),
+  dcc.Input(id = 'textbox2', value = 'ACGT', type = 'text'),
+
+  # remainder
   dcc.RadioItems(
     id='dropdown-a',
     options=[{'label': i, 'value': i} for i in ['Canada', 'USA', 'Mexico']],
@@ -24,7 +38,9 @@ app.layout = html.Div([
 
 ])
 
-
+##
+# Callbacks
+##
 @app.callback(
   dash.dependencies.Output('output-a', 'children'),
   [dash.dependencies.Input('dropdown-a', 'value')])
@@ -38,6 +54,9 @@ def callback_a(dropdown_value):
 def callback_b(dropdown_value):
   return 'You\'ve selected "{}"'.format(dropdown_value)
 
+
+# CSS
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 if __name__ == '__main__':
   app.run_server()
