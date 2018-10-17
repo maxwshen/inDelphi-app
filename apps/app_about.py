ea58fcee08efc0c8183c1abbd7be0199a5eb34b6
maxwshen <maxwshen@gmail.com>
2018-10-17 18:36:40 -0400

    about page

--- apps/app_about.py
+++ apps/app_about.py
@@ -79,18 +79,77 @@ layout = html.Div([
           [
             html.Div(
               [
-                dcc.Markdown(dedent('''
-                  This website provides an online interactive implementation of inDelphi, a computational method that predicts DNA repair outcomes at DNA double strand breaks induced by CRISPR/SpCas9 resulting from non-homologous end-joining. inDelphi is described in:
-
-                  Max W. Shen*, Mandana Arbab*, Jonathan Y. Hsu, Daniel Worstell, Sannie J. Culbertson, Olga Krabbe, Christopher A. Cassa, David R. Liu, David K. Gifford, and Richard I. Sherwood. "Predictable and precise template-free editing of pathogenic variants." _Nature_, in press, 2018.
-
-                  This user guide page is intended as a resource for biologists using CRISPR. For a layperson-friendly description, please refer to our About page.
-                  '''),
-                  className = 'markdown_style',
+                html.Img(src = '/assets/fig-indel-len.gif'),
+                html.Div(
+                  [
+                    ''
+                  ],
+                  style = dict(
+                    fontStyle = 'italic',
+                    width = '450px',
+                    margin = '0 auto',
+                    marginBottom = '20px',
+                  )
                 ),
               ],
-              id = 'overview',
-              className = 'hashbuffer',
+              style = dict(
+                textAlign = 'center',
+              ),
+            ),
+
+            dcc.Markdown(dedent('''
+              inDelphi is a machine learning algorithm that is aimed to assist scientists using CRISPR. Here, we provide a layperson-friendly description of inDelphi. For a more scientifically accurate description, please refer to the User Guide page and our manuscript:
+
+              Max W. Shen*, Mandana Arbab*, Jonathan Y. Hsu, Daniel Worstell, Sannie J. Culbertson, Olga Krabbe, Christopher A. Cassa, David R. Liu, David K. Gifford, and Richard I. Sherwood. "Predictable and precise template-free editing of pathogenic variants." _Nature_, in press, 2018. 
+
+              #### Briefly...
+              '''),
+              className = 'markdown_style',
+            ),
+
+            html.Div(
+              [
+                html.Img(src = '/assets/fig-coverplus.PNG'),
+                html.Div(
+                  [
+                    ''
+                  ],
+                  style = dict(
+                    fontStyle = 'italic',
+                    width = '450px',
+                    margin = '0 auto',
+                    marginBottom = '20px',
+                  )
+                ),
+              ],
+              style = dict(
+                textAlign = 'center',
+              ),
+            ),
+
+            dcc.Markdown(dedent('''
+              CRISPR is a genome editing tool that has achieved widespread use by making it easier and cheaper than ever before to induce a DNA double-strand break at a precise location in the genome, specified by a user-designed guide RNA (gRNA). Double-strand breaks in the genome are traumatic to the cell as it will die if it cannot repair it. Cells repair this DNA double-strand break using a variety of _DNA repair pathways_.
+
+              _Homology-directed repair (HDR)_ is one DNA repair pathway that has been in the spotlight. In HDR, scientists can introduce a DNA template that looks similar to, but is not exactly the same as, the DNA surrounding the CRISPR cut. The cell will use this DNA template to repair the cut. The result is controllable editing of DNA at a precise location in the genome into user-specified DNA.
+
+              It turns out, however, that HDR is often less efficient than another DNA repair pathway called _non-homologous end-joining (NHEJ)_. In contrast to HDR which can be used to introduce user-specified edits, NHEJ has not been widely recognized as controllable. In the context of not only a traumatic DNA double-strand break, but CRISPR which will repeatedly cut the DNA, NHEJ can be thought of as a "willy-nilly", no-holds-barred effort to repair the cut into a form that CRISPR can no longer recut. The results are "ugly" insertions and deletions which are hugely diverse and heterogeneous in different cells. While HDR has been a golden child of genome editing, NHEJ has been viewed as undesirable noise that is unfortunately more efficient than HDR.
+
+              inDelphi is a computational model that predicts the heterogeneous (100+ unique) mixture of indels resulting from non-homologous end-joining at a CRISPR-induced cut. inDelphi synthesizes known biological mechanisms of DNA repair with machine learning and achieves strong accuracy.
+
+              In our study, we used inDelphi to identify a previously underappreciated class of disease-related mutations that could be corrected to its common, healthy DNA sequence, with high efficiency, using the "noisy" NHEJ pathway. We believe that our study suggests that, with inDelphi, NHEJ can be reinterpreted from an undesirable yet efficient DNA repair pathway, into an efficient DNA repair pathway (1) whose heterogeneity can be predicted and thereby controlled, and (2) that is useful for certain genome editing applications and .
+
+              #### Some applications:
+              - Identify gRNAs that have extremely high frameshift frequencies to more efficiently knock down genes
+              - Identify gRNAs that precisely and efficiently delete specific sequences, such as splicing elements
+              - Identify gRNAs with highly precise indel outcomes, to consolidate the risk of undesired NHEJ editing in an HDR application
+              - Identify gRNAs with imprecise indel outcomes to boost the information content of CRISPR scars for lineage tracing
+              - Depending on the nearby sequence context, identify gRNAs that can introduce a specific desired indel more efficiently than HDR
+
+              #### Credit
+              The inDelphi website was built by Max W. Shen using Python, Dash and Plotly. You can contact him at maxwshen (at) mit.edu.
+
+              '''),
+              className = 'markdown_style',
             ),
 
           ],
